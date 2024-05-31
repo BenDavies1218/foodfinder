d6ebc52b24f35743cb17f7c6d5bdad4dcc81d8bc
Willem Gibson <129262358+WillemGibson@users.noreply.github.com>
2024-05-31 17:07:45 +1000

    fix: search button + typo

--- src/pages/FavouritesPage.jsx
+++ src/pages/FavouritesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import FoodFavouritesItems from "../components/FoodFavouriteItems";
 
 export default function FavouritePage() {
